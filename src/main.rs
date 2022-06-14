791315e194ab0c5869b906ef25842d3e7d99c707
browsky300 <32629291+browsky300@users.noreply.github.com>
2022-06-14 11:09:38 -0700

    add the enter_to_continue function (amazing code)
    i am very good at rust

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use std::io;
 
 fn main() {
     println!("THE ROOMS\n\nsimple text adventure game in Rust.\n\nPRESS ENTER TO START");
-    {
-        let mut goober = String::new();
-        io::stdin().read_line(&mut goober).expect("error 1");
+    enter_to_continue();
+	println!("You awake on a comfortable bed PLACEHOLDER TEXT PLACEHOLDER TEXT PLACEHOLDER TEXT PLACEHOLDER TEXT\nPLACEHOLDER TEXT PLACEHOLDER TEXT PLACEHOLDER TEXT PLACEHOLDER TEXT\nPLACEHOLDER TEXT PLACEHOLDER TEXT PLACEHOLDER TEXT PLACEHOLDER TEXT\nPLACEHOLDER TEXT PLACEHOLDER TEXT PLACEHOLDER TEXT PLACEHOLDER TEXT\n\nWhat do you do?");
+	
+}
 
-    }
+fn enter_to_continue() {
+	{
+		let mut goober = String::new(); // goofy goober code
+        io::stdin().read_line(&mut goober).expect("error 1");
+		// i am not very good at rust
+	}
 }
