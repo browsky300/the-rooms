03318e0fb1244d429d88b8aaa5585ebb1853a6ab
browsky300 <32629291+browsky300@users.noreply.github.com>
2022-06-15 11:13:59 -0700

    added response printer and other stuff
    print_response prints a text based on the stage id provided

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,46 @@
-use std::io;
+
+
+use std::io; // for read line
 
 fn main() {
 
-	let mut stageID = 0i8;
+	let mut stage_id = 0i8; // each stage has its own number, a room may have more than one stage.
+	let mut player_input = String::from("");
 
-	
-    println!("THE ROOMS\n\nsimple text adventure game in Rust.\n\nPRESS ENTER TO START");
+
+
+
+
+	print_response(stage_id);
     enter_to_continue();
-	println!("You awake on a comfortable bed PLACEHOLDER TEXT\n\nWhat do you do?");
+
+	stage_id = 1;
+	print_response(stage_id);
+	io::stdin().read_line(&mut player_input).expect("error");
+	text_parser(player_input);
 	
 }
 
 fn enter_to_continue() {
 	{
-		let mut goober = String::new(); // goofy goober code
-        io::stdin().read_line(&mut goober).expect("error 1");
+		let mut goober = String::new(); // goofy code
+        io::stdin().read_line(&mut goober).expect("error");
 		// i am not very good at rust
 	}
 }
 
-fn text_parser(inputString) {
+#[allow(unused_variables)]
+fn text_parser(input_string: String) {
+	
+}
 
+fn print_response(n: i8) {
+	let mut _response = String::from("");
+	match n {
+		0 => _response = "THE ROOMS\n\nsimple text adventure game in Rust.\n\nPRESS ENTER TO START".to_string(),
+		1 => _response = "You awake on a comfortable bed PLACEHOLDER TEXT\n\nWhat do you do?".to_string(),
+		2 => _response = "You have found the key to open the door.\n\nWhat do you do?".to_string(),
+		_ => _response = "INVALID STAGE ID".to_string(),
+	}
+	println!("{}\n", _response);
 }
