823aa858801de5a25d64e56febf51ff804626299
browsky300 <32629291+browsky300@users.noreply.github.com>
2022-06-24 11:29:47 -0700

    IT WORKS NOW

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,20 @@
-#![allow(dead_code)]
-#![allow(unused_variables)] // all of these are temporary and are used while the main() function isnt finished, REMOVE THESE LATER
-
 use std::io; // for read line
 use std::str;
 
 fn main() { // real
 
 	let mut stage_id = 0u8; // each stage has its own number, a room may have more than one stage.
-	let mut player_input = String::from("");
-	let mut parsed_input_arr = [String::new(), String::new()];
-	let mut word1 = String::new()
-	let mut word2 = String::new()
-	//let mut return_array
-
-
-
 	loop { // this is the main game loop
+		let mut player_input = String::from("");
 		if stage_id == 255 {
 			break;
 		};
 		print_response(stage_id); // print text based on the stage id
 		io::stdin().read_line(&mut player_input).expect("error"); // read player input
-		parsed_input_arr = text_parser(player_input.clone(), stage_id);
-		word1 = parsed_input_arr[0].clone();
-		word2 = parsed_input_arr[1].clone();
-		println!("word 1 is ({}), word2 is ({})", word1, word2);
-		stage_id = logic_decider(word1.as_str(), word1.as_str(), stage_id); // send the stage id and input data to the logic decider which will figure out what the player wanted to say and modify the stage ID based on action
+		let split = player_input.split(" ");
+		let mut ivec = split.collect::<Vec<&str>>();
+		ivec.push("filler");
+		stage_id = logic_decider(ivec[0].clone().trim(), ivec[1].clone().trim(), stage_id);
 	}
 
 	println!("Thanks for playing!");
@@ -41,23 +30,14 @@ fn enter_to_continue() {
 	};
 }
 
-fn text_parser(input_ref: String, stage_id: u8) -> [String; 2] {
-	let input_copy = input_ref.clone();
-	let split = input_copy.split(" ");
-	let mut input_vec: Vec<&str> = split.collect(); // this is bad but i dont care
-	input_vec.push("invalid");
-	input_vec.push("invalid");
-	return [String::from(input_vec[0]), String::from(input_vec[1])];
-}
-
-fn print_response(n: u8) { // refer to this to find out what the stage numbers mean
-	match n {
-		0 => println!("THE ROOMS\n\nsimple text adventure game in Rust.\n\nPRESS ENTER TO START"),
-		1 => println!("\nYou awake on a comfortable bed PLACEHOLDER TEXT\n\nWhat do you do?"), // first room
-		2 => println!("\nYou find a key in between the couch cushions. It looks like it can be used to open the door.\n\nWhat do you do?"),
-		3 => println!("\n PLACEHOLDER TEXT WATCH THE NEWS\n\nWhat do you do?"), // second room
-		4 => println!("\nENTER CODE\n\n"),
-		5 => println!("\nTHIRD ROOM PLACEHOLDER TEXT\n\n"),
+fn print_response(id: u8) { // refer to this to find out what the stage numbers mean
+	match id {
+		0 => println!("THE ROOMS\n\nsimple text adventure game in Rust.\n\nPRESS ENTER TO START\n"),
+		1 => println!("\nWhat do you do?\n"), // first room
+		2 => println!("\nIt looks like this key can be used to open the door.\n\nWhat do you do?\n"),
+		3 => println!("\nWhat do you do?\n"), // second room
+		4 => println!("\nENTER CODE\n"),
+		5 => println!("\nTHIRD ROOM PLACEHOLDER TEXT\n"),
 		_ => println!("INVALID STAGE ID")
 	};
 }
@@ -65,7 +45,8 @@ fn print_response(n: u8) { // refer to this to find out what the stage numbers m
 fn logic_decider(w1: &str, w2: &str, id: u8) -> u8 {
 	println!();
 	match id {
-		0 => return 1,
+		0 => {println!("\nYou awake on a comfortable bed, but find yourself in an\nuncomfortable room that you don't recognize. In it is the bed,\na beige couch, a tall lamp, and a wooden door on the wall.");
+		return 1;}
 		1 => match w1 {
 			"search" | "look" | "check" | "see" => match w2 {
 				"bed" => {println!("There is nothing under the bed.");
@@ -102,7 +83,7 @@ fn logic_decider(w1: &str, w2: &str, id: u8) -> u8 {
 				return id;}
 			},
 			"open" => match w2 {
-				"door" => {println!("You insert the key into the door, and it swings open.");
+				"door" => {println!("You insert the key into the door, and it swings open. Beyond the door is a ");
 				return 3;},
 				_ => {println!("Can't do that!");
 				return id;}
@@ -129,18 +110,21 @@ fn logic_decider(w1: &str, w2: &str, id: u8) -> u8 {
 			_ => {println!("CODE INCORRECT");
 			return 3;}
 		},
-		_ => {println!("ERROR INVALID STAGE ID, this should not happen");
-		return id;}
 		5 => match w1 {
 			"search" | "look" | "check" | "see" => match w2 {
 				"bomb" => {println!("A bomb with a keypad. It's better not to touch this right now.");
-				return id;}
+				return id;},
 				"briefcase" => {println!("A locked briefcase. There is a keyhole on the outside.");
-				return id;}
+				return id;},
 				"statue" => {println!("A bronze statue. Next to it there is a nameplate that says 'Goliath'. It doesn't seem like it will help with getting out of here.");
 				return id;},
-
-			}
-		}
+				_ => {println!("Can't search that!");
+				return id;}
+			},
+			_ => {println!("Can't do that!");
+			return id;}
+		},
+		_ => {println!("ERROR INVALID STAGE ID, this should not happen");
+		return id;}	
 	};
 }
